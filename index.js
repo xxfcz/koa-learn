f7e272948db2889edb8d1cdc54cb6b927b3c3d45
xxfcz <xxf_cz@163.com>
2018-06-11 23:24:00 +0800

    +) route, static, error handling

--- index.js
+++ index.js
@@ -1,8 +1,64 @@
-const Koa = require('koa');
-const app = new Koa();
+const Koa = require('koa')
+const app = new Koa()
+const fs = require('fs')
+const route = require('koa-route')
+const static = require('koa-static')
+const path = require('path')
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
+app.use(static(path.join(__dirname, 'static')))
 
-app.listen(3000);
+const logger = (ctx, next) => {
+  console.log(`${Date.now()} ${ctx.request.method} ${ctx.request.url}`)
+  next()
+}
+
+app.use(logger)
+
+const error_handler = async (ctx, next) => {
+  try {
+    await next()
+  } catch (err) {
+    ctx.response.status = err.statusCode || err.status || 500
+    if (ctx.accepts('text')) ctx.response.body = err.status + ' ' + err.message
+    else if (ctx.accepts('json'))
+      ctx.response.body = {
+        message: err.message
+      }
+    else ctx.response.body = err.status + ' ' + err.message
+  }
+}
+
+app.use(error_handler)
+
+const main = ctx => {
+  ctx.response.body = '<h1>首页</h1><div><a href="/api">API</a></div>'
+}
+
+const api = async ctx => {
+  if (ctx.accepts('xml')) {
+    ctx.response.type = 'xml'
+    ctx.body = '<data>Hello World</data>'
+  } else if (ctx.accepts('json')) {
+    ctx.response.type = 'json'
+    ctx.body = { data: 'Hello, World' }
+  } else if (ctx.accepts('html')) {
+    ctx.response.type = 'html'
+    ctx.body = await fs.createReadStream('template.html')
+  } else {
+    ctx.response.type = 'text'
+    ctx.body = 'Hello World'
+  }
+}
+
+const error = ctx => {
+  // ctx.response.status = 500
+  // ctx.response.body = "Error"
+  ctx.throw(500)
+}
+
+app.use(route.get('/', main))
+app.use(route.get('/api', api))
+app.use(route.get('/error', error))
+
+app.listen(3000)
+console.log('Listening on port 3000...')
